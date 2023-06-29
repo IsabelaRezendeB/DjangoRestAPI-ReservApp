8d4ea18c7c60efc7201446cfe23466e449b93bbd
Isabela <belarbarbosa@gmail.com>
2023-06-29 13:54:33 -0300

    Limpeza do código

--- reservapp/views.py
+++ reservapp/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, filters, generics
 from django_filters.rest_framework import DjangoFilterBackend
 from reservapp.models import Usuario, Restaurante, Reserva, RestauranteFavorito, ItemCardapio
